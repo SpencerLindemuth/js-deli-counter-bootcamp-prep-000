0d8f1198bc65dddfa79d5dbbee3bef14e6dc6187
Spencer Lindemuth <lindemut@gmail.com>
2019-03-12 21:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, person){
 
 function nowServing(line){
   if(!line.length){
-    return 'There is nobody waiting to be served!'
+    return `There is nobody waiting to be served!`
   }
   else{
     return `Currently serving ${line.shift()}.`
@@ -14,21 +14,18 @@ function nowServing(line){
 
 function currentLine(line){
   if(!line.length){
-      return 'The line is currently empty.'
-    }
-    else{
-      var string = "The line is currently: "
-      for(var i = 0; i < line.length; i++){
-        if(line.length !== i + 1){
-          string = string + `${i + 1}. ${line[i]}, `
-        }
-        else{
-          string = string + `${i + 1}. ${line[i]}`
-        }
-      }
-      return string
-    }
+    return `The line is currently empty.`
   }
-
-var deli= ["Jason", "Freddy", "Michael Meyers"]
-console.log(currentLine(deli))
+  else{
+    var queue = "The line is currently: "
+    for(let i = 0; i < line.length; i++){
+      if(i < (line.length - 1)){
+        queue = queue + `${i + 1}. ${line[i]}, `
+      }
+      else{
+        queue = queue + `${i + 1}. ${line[i]}`
+      }
+    }
+    return queue
+  }
+}
